510b29619f89126cb4b7b029a9a53c7b1c4cb299
NRT03 <neamulnasif@gmail.com>
2025-08-09 04:10:06 +0600

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,45 @@
-const CACHE_NAME = 'nrt-v2';
+const CACHE_NAME = 'nrt-v6'; // Change this number every time you update your app
 const urlsToCache = [
   './',
   'index.html',
   'manifest.json',
-  'https://cdn.tailwindcss.com',
-  'https://cdn.jsdelivr.net/npm/chart.js',
+  'service-worker.js',
+  'https://cdn.jsdelivr.net/npm/chart.js@3.7.0/dist/chart.min.js',
   'https://html2canvas.hertzen.com/dist/html2canvas.min.js',
   'icon-192x192.png',
   'icon-512x512.png'
 ];
 
 self.addEventListener('install', event => {
+  console.log('Service Worker: Installing...');
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
-        console.log('Opened cache');
+        console.log('Service Worker: Caching new files.');
         return cache.addAll(urlsToCache);
       })
+      .then(() => {
+        return self.skipWaiting(); // Force the new service worker to activate immediately
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  console.log('Service Worker: Activating and cleaning up old caches.');
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            console.log('Service Worker: Deleting old cache ' + cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => {
+        return clients.claim(); // Take control of all pages immediately
+    })
   );
 });
 
@@ -29,20 +52,5 @@ self.addEventListener('fetch', event => {
         }
         return fetch(event.request);
       })
-  );
-});
-
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
   );
 });
